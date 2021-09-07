b191172e7ac2e305fc1d2076a746c5cc1c94e052
Yuri <yuri.shiba@unifesp.br>
2021-09-07 02:06:55 -0300

    Created basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import { DataTable } from "components/DataTable";
+import { Footer } from "components/Footer";
+import { NavBar } from "components/NavBar";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Olá mundo</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
